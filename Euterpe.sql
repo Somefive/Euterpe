f15b697e28dddccae79f89d1b43c304320039757
WuHanghang <wuxx14@mails.tsinghua.edu.cn>
2016-03-09 00:26:57 +0800

    更新数据库

--- Euterpe.sql
+++ Euterpe.sql
@@ -1,27 +1,24 @@
--- MySQL dump 10.13  Distrib 5.6.27, for Win64 (x86_64)
---
--- Host: localhost    Database: Euterpe
--- ------------------------------------------------------
--- Server version	5.6.27
+/*
+Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+Source Server         : localhost_3306
+Source Server Version : 50628
+Source Host           : localhost:3306
+Source Database       : euterpe
 
---
--- Table structure for table `composition`
---
+Target Server Type    : MYSQL
+Target Server Version : 50628
+File Encoding         : 65001
 
+Date: 2016-03-09 00:25:30
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for composition
+-- ----------------------------
 DROP TABLE IF EXISTS `composition`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `composition` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `studentid` int(11) DEFAULT NULL,
@@ -34,73 +31,81 @@ CREATE TABLE `composition` (
   `date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `composition`
---
+-- ----------------------------
+-- Records of composition
+-- ----------------------------
 
-LOCK TABLES `composition` WRITE;
-/*!40000 ALTER TABLE `composition` DISABLE KEYS */;
-/*!40000 ALTER TABLE `composition` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `course`
---
-
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
 DROP TABLE IF EXISTS `course`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `course` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `description` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `course`
---
+-- ----------------------------
+-- Records of course
+-- ----------------------------
+INSERT INTO `course` VALUES ('1', 'TestCourse', 'This course is opened for backend test.');
+INSERT INTO `course` VALUES ('2', 'AnotherTestCourse', 'Another course opened for test.');
+INSERT INTO `course` VALUES ('3', 'NewCourse', 'Hello World');
 
-LOCK TABLES `course` WRITE;
-/*!40000 ALTER TABLE `course` DISABLE KEYS */;
-INSERT INTO `course` VALUES (1,'TestCourse','This course is opened for backend test.'),(2,'AnotherTestCourse','Another course opened for test.'),(3,'NewCourse','Hello World');
-/*!40000 ALTER TABLE `course` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `courseenrollment`
---
-
+-- ----------------------------
+-- Table structure for courseenrollment
+-- ----------------------------
 DROP TABLE IF EXISTS `courseenrollment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `courseenrollment` (
   `courseid` int(11) NOT NULL,
   `studentid` int(11) NOT NULL,
   `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `courseenrollment`
---
+-- ----------------------------
+-- Records of courseenrollment
+-- ----------------------------
+INSERT INTO `courseenrollment` VALUES ('1', '2', '0000-00-00');
+INSERT INTO `courseenrollment` VALUES ('2', '2', null);
+INSERT INTO `courseenrollment` VALUES ('1', '1', null);
+INSERT INTO `courseenrollment` VALUES ('1', '4', null);
+INSERT INTO `courseenrollment` VALUES ('1', '5', null);
 
-LOCK TABLES `courseenrollment` WRITE;
-/*!40000 ALTER TABLE `courseenrollment` DISABLE KEYS */;
-INSERT INTO `courseenrollment` VALUES (1,2,'0000-00-00'),(2,2,NULL),(1,1,NULL),(1,4,NULL);
-/*!40000 ALTER TABLE `courseenrollment` ENABLE KEYS */;
-UNLOCK TABLES;
+-- ----------------------------
+-- Table structure for post
+-- ----------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `postId` int(11) NOT NULL AUTO_INCREMENT,
+  `postManId` int(11) DEFAULT '0',
+  `time` datetime DEFAULT NULL,
+  `title` text COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `readMenList` text COLLATE utf8_unicode_ci,
+  `likeMenList` text COLLATE utf8_unicode_ci COMMENT '点赞人的名字,用|分隔开',
+  `simpleInfo` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`postId`),
+  KEY `postId` (`postId`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Table structure for table `studentbasicinformation`
---
+-- ----------------------------
+-- Records of post
+-- ----------------------------
+INSERT INTO `post` VALUES ('0', '5', '2016-03-08 20:44:43', '测试精简信息', '<p><i><tt>string</tt></i></p><dd><p>输入字符串。</p></dd><dt><span class=\"term\"><i><tt>start</tt></i></span></dt><dd><p>如果 <i><tt>start</tt></i> 是非负数，返回的字符串将从 <i><tt>string</tt></i> 的 <i><tt>start</tt></i> 位置开始，从 0 开始计算。例如，在字符串 “<i>abcdef</i>” 中，在位置 <i>0</i> 的字符是 “<i>a</i>”，位置 <i>2</i> 的字符串是 “<i>c</i>” 等等。</p><p>如果 <i><tt>start</tt></i> 是负数，返回的字符串将从 <i><tt>string</tt></i> 结尾处向前数第 <i><tt>start</tt></i> 个字符开始。</p><p>如果 <i><tt>string</tt></i> 的长度小于或等于</p></dd>', '1|2|3', null, '5|吴行行|测试精简信息|<p><i><tt>string</tt></i></p><dd><p>输入字符串。</p></dd><dt><span class=\"term\"><i><tt>start</tt></i></span></dt><dd><p>如果 <i><tt>start</tt></i> 是非负数，返回的字符串将从 <i><tt>string</tt></i> 的 <i><tt>start</tt></i> 位置开始，从 0 开始计算。例如，在字符串 “<i>abcdef</i>” 中，在位置 <i>0</i> 的字符是 “<i>a</i>”，位置 <i>2</i> 的字符串是 “<i>c</i>” 等等。</p><p>如果 <i><tt>start</tt></i> 是负数，返回的字符串将从 <i><tt>string</tt></i> 结尾处向前数第 <i><tt>start</tt></i> 个字符开始。</p><p>如果 <i><tt>string</tt></i> 的长度小于或等于</p></dd>|2016-03-08');
+INSERT INTO `post` VALUES ('22', '5', '2016-03-08 21:09:43', '测试主键的递增', '<p>会不会递增呀好伤心。</p><p>下面是乱写的。。。</p><p>123456</p>', null, null, '5|吴行行|测试主键的递增|<p>会不会递增呀好伤心。</p><p>下面是乱写的。。。</p><p>123456</p>|2016-03-08');
+INSERT INTO `post` VALUES ('23', '5', '2016-03-08 21:11:10', '测试图片上传', '<p>来个图片</p><p>来个图片<br></p><p>来个图片<span class=\"redactor-invisible-space\"><br></span></p><p><span class=\"redactor-invisible-space\">来个图片<span class=\"redactor-invisible-space\"><br></span></span></p><p><span class=\"redactor-invisible-space\"><span class=\"redactor-invisible-space\"><img src=\"/uploads/5/7a07e25c31-u-98798092877674487fm-116gp-0.jpg\"><span class=\"redactor-invisible-space\"><br></span></span></span></p>', '1|5', null, '5|吴行行|测试图片上传|<p>来个图片</p><p>来个图片<br></p><p>来个图片<span class=\"redactor-invisible-space\"><br></span></p><p><span class=\"redactor-invisible-space\">来个图片<span class=\"redactor-invisible-space\"><br></span></span></p><p><span class=\"redactor-invisible-space\"><span class=\"redactor-invisible-space\"><img src=\"/uploads/5/7a07e25c31-u-98798092877674487fm-116gp-0.jpg\"><span class=\"redactor-invisible-space\"><br></span></span></span></p>|2016-03-08');
+INSERT INTO `post` VALUES ('24', '5', '2016-03-08 21:20:30', '第4个帖子', '<p>为啥写这个帖子，不知道</p>', null, null, '5|吴行行|第4个帖子|<p>为啥写这个帖子，不知道</p>|2016-03-08');
+INSERT INTO `post` VALUES ('25', '5', '2016-03-08 21:52:20', '马上要出操了', '<p>好伤心，根本不想出操！根本不想！！</p>', null, null, '5|吴行行|马上要出操了|<p>好伤心，根本不想出操！根本不想！！</p>|2016-03-08');
+INSERT INTO `post` VALUES ('26', '5', '2016-03-08 22:49:17', '再来一个帖子', '<ul><li>能够看到滚动条的效果了！好开心好开心！<a href=\"http://localhost:8080/course/discussion/discussion\">我的地址！</a></li></ul>', null, null, '5|吴行行|再来一个帖子|<ul><li>能够看到滚动条的效果了！好开心好开心！<a href=\"http://localhost:8080/course/discussion/discussion\">我的地址！</a></li></ul>|2016-03-08');
+INSERT INTO `post` VALUES ('27', '5', '2016-03-08 23:19:10', '测试', '<p><img src=\"/uploads/5/0ec2ae1d04-u-98798092877674487fm-116gp-0.jpg\"></p>', null, null, '5|吴行行|测试|<p><img src=\"/uploads/5/0ec2ae1d04-u-98798092877674487fm-116gp-0.jpg\"></p>|2016-03-08');
+INSERT INTO `post` VALUES ('28', '5', '2016-03-09 00:14:59', '最后测试一次！', '<p>明天就要给老师展示了好紧张，最后测试一次！</p><p>我先测试一个图片哈</p><p><img src=\"/uploads/5/086c98cd8f-imgfortest.jpg\"></p><hr><p>再来一个链接，点击它会跳转<a href=\"http://localhost:8080/course/discussion/discussion\" target=\"_blank\">我的讨论区！</a></p><p>换个颜色！<span style=\"color: rgb(192, 80, 77);\">这是红色!</span></p>', null, null, '5|吴行行|最后测试一次！|<p>明天就要给老师展示了好紧张，最后测试一次！</p><p>我先测试一个图片哈</p><p><img src=\"/uploads/5/086c98cd8f-imgfortest.jpg\"></p><hr><p>再来一个链接，点击它会跳转<a href=\"http://localhost:8080/course/discussion/discussion\" target=\"_blank\">我的讨论区！</a></p><p>换个颜色！<span style=\"color: rgb(192, 80, 77);\">这是红色!</span></p>|2016-03-09');
 
+-- ----------------------------
+-- Table structure for studentbasicinformation
+-- ----------------------------
 DROP TABLE IF EXISTS `studentbasicinformation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `studentbasicinformation` (
   `id` int(11) NOT NULL,
   `school` varchar(45) DEFAULT 'None',
@@ -112,25 +117,18 @@ CREATE TABLE `studentbasicinformation` (
   `verify` varchar(5) DEFAULT 'N',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `studentbasicinformation`
---
+-- ----------------------------
+-- Records of studentbasicinformation
+-- ----------------------------
+INSERT INTO `studentbasicinformation` VALUES ('1', 'None', null, null, null, 'femal', null, 'N');
+INSERT INTO `studentbasicinformation` VALUES ('2', 'Tsinghua', '2014000000', '学生', 'default student', 'male', '120', 'N');
+INSERT INTO `studentbasicinformation` VALUES ('3', 'Tsinghua', '2014000000', '机器人', 'Robot Student', 'male', '123456', 'N');
 
-LOCK TABLES `studentbasicinformation` WRITE;
-/*!40000 ALTER TABLE `studentbasicinformation` DISABLE KEYS */;
-INSERT INTO `studentbasicinformation` VALUES (1,'None',NULL,NULL,NULL,'femal',NULL,'N'),(2,'Tsinghua','2014000000','学生','default student','male','120','N'),(3,'Tsinghua','2014000000','机器人','Robot Student','male','123456','N');
-/*!40000 ALTER TABLE `studentbasicinformation` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
@@ -140,26 +138,16 @@ CREATE TABLE `user` (
   `email` varchar(45) NOT NULL DEFAULT '',
   `type` varchar(45) NOT NULL DEFAULT 'Student',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Teacher','8ab7bbdf01a24e988c50c4cfe9557814','','','','Teacher'),(2,'Student','f5c0a1c9384c2e25e79ba1abf5d9a037','','','student@mails.tsinghua.edu.cn','Student'),(3,'RobotStudent','2e4a1167ef74ccb04890d01da97602c7','','','robotstudent@mail.tsinghua.edu.cn','Student'),(4,'陈光斌','e10adc3949ba59abbe56e057f20f883e','','','cgb14@mails.tsinghua.edu.cn','Student');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-02-23 21:17:24
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'Teacher', '8ab7bbdf01a24e988c50c4cfe9557814', '', '', '', 'Teacher');
+INSERT INTO `user` VALUES ('2', 'Student', 'f5c0a1c9384c2e25e79ba1abf5d9a037', '', '', 'student@mails.tsinghua.edu.cn', 'Student');
+INSERT INTO `user` VALUES ('3', 'RobotStudent', '2e4a1167ef74ccb04890d01da97602c7', '', '', 'robotstudent@mail.tsinghua.edu.cn', 'Student');
+INSERT INTO `user` VALUES ('4', '陈光斌', 'e10adc3949ba59abbe56e057f20f883e', '', '', 'cgb14@mails.tsinghua.edu.cn', 'Student');
+INSERT INTO `user` VALUES ('5', '吴行行', 'e807f1fcf82d132f9bb018ca6738a19f', '', '', 'wuxx14@mails.tsinghua.edu.cn', 'Student');
+INSERT INTO `user` VALUES ('6', 'test', 'e10adc3949ba59abbe56e057f20f883e', '', '', '123@dwq.com', 'Student');
+INSERT INTO `user` VALUES ('7', 'test2', 'e10adc3949ba59abbe56e057f20f883e', '', '', '213@da.com', 'Student');
+INSERT INTO `user` VALUES ('8', '吴行行1', 'e10adc3949ba59abbe56e057f20f883e', '', '', '12@312.com', 'Student');
