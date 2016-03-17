6b31e871f02e75c41c903664971fb5b8c906b18e
WuHanghang <wuxx14@mails.tsinghua.edu.cn>
2016-03-17 23:40:13 +0800

    数据库更新

--- Euterpe.sql
+++ Euterpe.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50628
 File Encoding         : 65001
 
-Date: 2016-03-17 23:16:37
+Date: 2016-03-17 23:39:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -113,16 +113,15 @@ CREATE TABLE `post` (
   `isPost` int(1) DEFAULT NULL,
   PRIMARY KEY (`postId`),
   KEY `postId` (`postId`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of post
 -- ----------------------------
-INSERT INTO `post` VALUES ('22', '5', '2016-03-08 21:09:43', '', '<p>会不会递增呀好伤心。</p><p>下面是乱写的。。。</p><p>123456</p>', '5', null, '吴行行', '0', '0', '49', '1');
-INSERT INTO `post` VALUES ('24', '9', '2016-03-08 21:20:30', '第4个帖子', '<p>为啥写这个帖子，不知道</p>', '9|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|6|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5', '1', '9|ykd|第4个帖子|<p>为啥写这个帖子，不知道</p>|2016-03-08|1|2', '1', '2', '22|35|43|44|45', '0');
+INSERT INTO `post` VALUES ('22', '5', '2016-03-08 21:09:43', '', '<p>会不会递增呀好伤心。</p><p>下面是乱写的。。。</p><p>123456</p>', '5', null, '吴行行', '0', '0', '49|50', '1');
+INSERT INTO `post` VALUES ('24', '9', '2016-03-08 21:20:30', '第4个帖子', '<p>为啥写这个帖子，不知道</p>', '9|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|6|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5|5', '1|5', '9|ykd|第4个帖子|<p>为啥写这个帖子，不知道</p>|2016-03-08|1|2', '1', '2', '22|35|43|44|45', '0');
 INSERT INTO `post` VALUES ('25', '5', '2016-03-08 21:52:20', '马上要出操了', '<p>好伤心，根本不想出操！根本不想！！</p>', '|9|9|9|9|9|9|9', null, '5|吴行行|马上要出操了|<p>好伤心，根本不想出操！根本不想！！</p>|2016-03-08|0|0', '0', '0', '|36', '0');
 INSERT INTO `post` VALUES ('26', '5', '2016-03-08 22:49:17', '再来一个帖子', '<ul><li>能够看到滚动条的效果了！好开心好开心！<a href=\"http://localhost:8080/course/discussion/discussion\">我的地址！</a></li></ul>', '5|9|9|9|9|9|9|9|9|9|9|9|9', '1|2', '5|吴行行|再来一个帖子|<ul><li>能够看到滚动条的效果了！好开心好开心！<a href=\"http://localhost:8080/course/discussion/discussion\">我的地址！</a></li></ul>|2016-03-08|0|2', '0', '2', '|46', '0');
-INSERT INTO `post` VALUES ('27', '9', '2016-03-08 23:19:10', '测试', '<p><img src=\"/uploads/5/0ec2ae1d04-u-98798092877674487fm-116gp-0.jpg\"></p>', '9|5|5|5', null, '9|ykd|测试|<p><img src=\"/uploads/5/0ec2ae1d04-u-98798092877674487fm-116gp-0.jpg\"></p>|2016-03-08|1|0', '1', '0', null, '0');
 INSERT INTO `post` VALUES ('28', '5', '2016-03-09 00:14:59', '最后测试一次！', '<p>明天就要给老师展示了好紧张，最后测试一次！</p><p>我先测试一个图片哈</p><p><img src=\"/uploads/5/086c98cd8f-imgfortest.jpg\"></p><hr><p>再来一个链接，点击它会跳转<a href=\"http://localhost:8080/course/discussion/discussion\" target=\"_blank\">我的讨论区！</a></p><p>换个颜色！<span style=\"color: rgb(192, 80, 77);\">这是红色!</span></p>', '5|9|9|9', null, '5|吴行行|最后测试一次！|<p>明天就要给老师展示了好紧张，最后测试一次！</p><p>我先测试一个图片哈</p><p><img src=\"/uploads/5/086c98cd8f-imgfortest.jpg\"></p><hr><p>再来一个链接，点击它会跳转<a href=\"http://localhost:8080/course/discussion/discussion\" target=\"_blank\">我的讨论区！</a></p><p>换个颜色！<span style=\"color: rgb(192, 80, 77);\">这是红色!</span></p>|2016-03-09|0|0', '0', '0', null, '0');
 INSERT INTO `post` VALUES ('29', '5', '2016-03-09 00:24:43', '测试精简信息', '<p><i><tt>string</tt></i></p><dd><p>输入字符串。</p></dd><dt><span class=\"term\"><i><tt>start</tt></i></span></dt><dd><p>如果 <i><tt>start</tt></i> 是非负数，返回的字符串将从 <i><tt>string</tt></i> 的 <i><tt>start</tt></i> 位置开始，从 0 开始计算。例如，在字符串 “<i>abcdef</i>” 中，在位置 <i>0</i> 的字符是 “<i>a</i>”，位置 <i>2</i> 的字符串是 “<i>c</i>” 等等。</p><p>如果 <i><tt>start</tt></i> 是负数，返回的字符串将从 <i><tt>string</tt></i> 结尾处向前数第 <i><tt>start</tt></i> 个字符开始。</p><p>如果 <i><tt>string</tt></i> 的长度小于或等于</p></dd>', '1|2|3|5|9|9', '1|2', '5|吴行行|测试精简信息|<p><i><tt>string</tt></i></p><dd><p>输入字符串。</p></dd><dt><span class=\"term\"><i><tt>start</tt></i></span></dt><dd><p>如果 <i><tt>start</tt></i> 是非负数，返回的字符串将从 <i><tt>string</tt></i> 的 <i><tt>start</tt></i> 位置开始，从 0 开始计算。例如，在字符串 “<i>abcdef</i>” 中，在位置 <i>0</i> 的字符是 “<i>a</i>”，位置 <i>2</i> 的字符串是 “<i>c</i>” 等等。</p><p>如果 <i><tt>start</tt></i> 是负数，返回的字符串将从 <i><tt>string</tt></i> 结尾处向前数第 <i><tt>start</tt></i> 个字符开始。</p><p>如果 <i><tt>string</tt></i> 的长度小于或等于</p></dd>|2016-03-09|0|0', '0', '0', '|33', '0');
 INSERT INTO `post` VALUES ('31', '9', '2016-03-09 00:40:22', '学习学习', '<p>深夜党学习大神代码</p>', '9|5', null, '9|ykd|学习学习|<p>深夜党学习大神代码</p>|2016-03-09|0|0', '0', '0', null, '0');
@@ -141,6 +140,7 @@ INSERT INTO `post` VALUES ('44', '5', '2016-03-12 17:02:42', '', '<p>回复第�
 INSERT INTO `post` VALUES ('45', '5', '2016-03-13 00:09:53', '', '<p>我要回帖！能不能显示呀！我在测试回帖的显示!</p>', null, null, '吴行行', '0', '0', null, '1');
 INSERT INTO `post` VALUES ('46', '5', '2016-03-13 00:16:53', '', '<p>要保证每个帖子都有回帖！我瞎说的...</p>', null, null, '吴行行', '0', '0', null, '1');
 INSERT INTO `post` VALUES ('49', '5', '2016-03-17 23:07:18', '', '<p>测试气泡,测试气泡,<span class=\"redactor-invisible-space\">测试气泡,<span class=\"redactor-invisible-space\">测试气泡,<span class=\"redactor-invisible-space\">测试气泡,<span class=\"redactor-invisible-space\">测试气泡,<span class=\"redactor-invisible-space\">测试气泡,<span class=\"redactor-invisible-space\">测试气泡,<span class=\"redactor-invisible-space\"></span></span></span></span></span></span></span></p>', null, null, '吴行行', '0', '0', null, '2');
+INSERT INTO `post` VALUES ('50', '5', '2016-03-17 23:18:04', '', '<p>接下来还要测试气泡！测试气泡！怎么选一个好的颜色呀！</p>', null, null, '吴行行', '0', '0', null, '2');
 
 -- ----------------------------
 -- Table structure for studentbasicinformation
