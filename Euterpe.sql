efa9b90d5a450a7663cddf492b230d68219a2621
WuHanghang <wuxx14@mails.tsinghua.edu.cn>
2016-03-19 18:41:28 +0800

    就是很开心！我们要去庆祝下!

--- Euterpe.sql
+++ Euterpe.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost_3306
-Source Server Version : 50624
+Source Server Version : 50628
 Source Host           : localhost:3306
 Source Database       : euterpe
 
 Target Server Type    : MYSQL
-Target Server Version : 50624
+Target Server Version : 50628
 File Encoding         : 65001
 
-Date: 2016-03-19 14:00:35
+Date: 2016-03-19 18:40:44
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -158,7 +158,7 @@ CREATE TABLE `remind` (
 -- ----------------------------
 -- Records of remind
 -- ----------------------------
-INSERT INTO `remind` VALUES ('ykd', '9', '9:[12:13,14:1,9:9];2:[12:12]', null, null);
+INSERT INTO `remind` VALUES ('ykd', '9', '', null, null);
 
 -- ----------------------------
 -- Table structure for studentbasicinformation
